83c7ee304bd103d56582a45bade26ece375f13dd
amg2592 <almorga2592@gmail.com>
2022-11-03 19:08:10 -0600

    Se modifican escenas

--- src/scenes/SceneB.js
+++ src/scenes/SceneB.js
@@ -9,14 +9,10 @@ class SceneB extends Phaser.Scene{
         console.log('Escena SceneB');
     }
     
-    preload() {
-        this.load.path = './assets/';
-        this.load.image(['barraArriba1', 'barraArriba2', 'barraArriba3', 'barraArriba4', 'barraPuerta', 'puerta', 'barraCF1', 'barraCF2', 
-        'cuerda', 'torre1', 'torre2', 'torre3', 'torre4', 'torre5', 'barraElevador', 'picos', 'barraTiempo', 'escalar', 'barraDiagonal',
-        'escalera', 'ninja','fondo_opc1','coleccionable','end']);
-        this.load.audio('gong', ['./gong.mp3']);
-        this.load.audio('musicaFondo', ['./musicaFondo.mp3']);
-    }
+    // preload() {
+    //     this.load.path = './assets/';
+
+    // }
 
     create() {
         //MÚSICA
